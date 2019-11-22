01d4e2070e5fba29da5a6dda67d1b6c5027517ea
launcher <laun_cher@163.com>
2019-11-22 16:23:41 +0800

    项目搭建

--- gmall-user/src/main/java/com/ac/gmall/user/controller/UserController.java
+++ gmall-user/src/main/java/com/ac/gmall/user/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.ac.gmall.user.controller;
 
 import entity.UserMember;
-import org.hibernate.validator.constraints.EAN;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
